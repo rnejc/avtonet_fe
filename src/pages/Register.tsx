600fdfe3b21d9dbe981797586ef80872c820999d
Nejc Rodič <nejc.rodic@gmail.com>
2025-03-29 22:08:21 +0100

    Registracija

---
+++ src/pages/Register.tsx
@@ -0,0 +1,67 @@
+import { SyntheticEvent, useState } from "react";
+import axios from "axios";
+
+const Register = () => {
+    const [firstName, setFirstName] = useState("")
+    const [lastName, setLastName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [password2, setPassword2] = useState("")
+
+    const url = "http://localhost:3000/auth/register"
+    const [redirect, setRedirect] = useState(false);
+
+    const submit = async (e: SyntheticEvent) => {
+        e.preventDefault();
+
+        const data = {
+            firstName: firstName,
+            lastName: lastName,
+            email: email,
+            password: password,
+        }
+
+        console.log(data)
+
+        let res = await axios.post(url, data)
+
+
+    }
+
+    return (
+        <>
+            <div className="container">
+                <h2>Registracija</h2>
+                    <form onSubmit={submit}>
+                        <div className="form-floating">
+                            <input type="text" className="form-control" placeholder="Vstavi ime"
+                            id="firstNameInput" onChange={(e) => setFirstName(e.target.value)}/>
+                            <label htmlFor="firstNameInput">Ime</label>
+                        </div>
+                        <div className="form-floating">
+                            <input type="text" className="form-control" placeholder="Vstavi priimek"
+                            id="lastNameInput" onChange={(e) => setLastName(e.target.value)}/>
+                            <label htmlFor="lastNameInput">Priimek</label>
+                        </div>
+                        <div className="form-floating">
+                            <input type="email" className="form-control" placeholder="Vstavi e-pošto"
+                            id="emailInput" onChange={(e) => setEmail(e.target.value)}/>
+                            <label htmlFor="emailInput">E-Pošta</label>
+                        </div>
+                        <div className="form-floating">
+                            <input type="password" className="form-control" placeholder="Vstavi geslo"
+                            id="passwordInput" onChange={(e) => setPassword(e.target.value)}/>
+                            <label htmlFor="passwordInput">Geslo</label>
+                        </div>
+                        <div className="form-floating">
+                            <input type="password" className="form-control" placeholder="Ponovi geslo"
+                            id="password2Input" onChange={(e) => setPassword2(e.target.value)}/>
+                            <label htmlFor="password2Input">Geslo 2x</label>
+                        </div>
+                        <button type="submit" className="btn btn-primary">Registriraj</button>
+                    </form>
+            </div>
+        </>
+    )
+}
+export default Register;
